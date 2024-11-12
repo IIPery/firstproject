58519336fa639122d3d4f190ab14410402b34c30
iskander <pery3333@gmail.com>
2024-11-12 09:53:39 +0500

    Simple search created

--- goods/views.py
+++ goods/views.py
@@ -6,16 +6,21 @@ from pygments.lexer import default
 from setuptools.command.build_ext import if_dl
 
 from goods.models import Products
+from goods.utils import q_search
 
 
 # Create your views here.
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
+
     page = request.GET.get('page', 1)
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
     if category_slug == "all":
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
